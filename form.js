1cc0070f9639c8f171b7ebd817f684305133cba6
arlenekimp <58005233+arlenekimp@users.noreply.github.com>
2020-01-19 04:05:06 -0800

    adding changes from results

--- form.js
+++ form.js
@@ -1,9 +1,9 @@
 
-let data = {
-    name: "paw",
-    iMoney: 10,
-    eMoney: 100
-}
+// let data = {
+//     name: "paw",
+//     iMoney: 10,
+//     eMoney: 100
+// }
 function submit(){
     console.log("hesgd");
     let url = 'http://localhost:5500/results.html?data=' + JSON.stringify(data);
--- result.js
+++ result.js
@@ -1,22 +1,60 @@
 function load(){
     var url = new URL(window.location);
+    var iMoney = Number(url.searchParams.get("iMoney"));
     var eMoney = Number(url.searchParams.get("eMoney"));
-    console.log(eMoney);
+    console.log(iMoney);
 
-    if(eMoney < 200){
-        console.log('ajslkfdasldf');
-        document.getElementById('budget').innerHTML = "YOU're BROKE";
-        document.getElementById('image').style.display = 'block';
-    }
 
     // document.getElementById('budget').innerHTML = eMoney;
 
 }
 
-function feedme(){    
-    console.log('teststsdaf');
-}
+function updateMoney(){    
+    console.log('about to start');
+    var url = new URL(window.location);
+    var iMoney = Number(url.searchParams.get("iMoney"));
+    var eMoney = Number(url.searchParams.get("eMoney"));
 
-function starveme(){ 
+    iMoney += document.getElementById("update").value; 
 
+    if((iMoney/eMoney) < (0.1*eMoney)) {
+        console.log('stage 1');
+        document.getElementById('image1').style.display = 'block';
+    }
+    else if ((iMoney/eMoney) < (0.2*eMoney) && (iMoney/eMoney) > (0.1*eMoney)) {
+        console.log('stage 2');
+        document.getElementById('image2').style.display = 'block';
+    }
+    else if ((iMoney/eMoney) < (0.3*eMoney) && (iMoney/eMoney) > (0.2*eMoney)) {
+        // console.log('stage 3');
+        // document.getElementById('image2').style.display = 'block';
+    }
+    else if ((iMoney/eMoney) < (0.4*eMoney) && (iMoney/eMoney) > (0.3*eMoney)) {
+        // console.log('stage 4');
+        // document.getElementById('image2').style.display = 'block';
+    }
+    else if ((iMoney/eMoney) < (0.5*eMoney) && (iMoney/eMoney) > (0.4*eMoney)) {
+        // console.log('stage 5');
+        // document.getElementById('image2').style.display = 'block';
+    }
+    else if ((iMoney/eMoney) < (0.6*eMoney) && (iMoney/eMoney) > (0.5*eMoney)) {
+    //     console.log('stage 6');
+    //     document.getElementById('image2').style.display = 'block';
+    }
+    else if ((iMoney/eMoney) < (0.7*eMoney) && (iMoney/eMoney) > (0.6*eMoney)) {
+        // console.log('stage 7');
+        // document.getElementById('image2').style.display = 'block';
+    }
+    else if ((iMoney/eMoney) < (0.8*eMoney) && (iMoney/eMoney) > (0.7*eMoney)) {
+        // console.log('stage 8');
+        // document.getElementById('image2').style.display = 'block';
+    }
+    else if ((iMoney/eMoney) < (0.9*eMoney) && (iMoney/eMoney) > (0.8*eMoney)) {
+        // console.log('stage 9');
+        // document.getElementById('image2').style.display = 'block';
+    }
+    else {
+        // console.log('stage 10');
+        // document.getElementById('image2').style.display = 'block';
+    }
 }
