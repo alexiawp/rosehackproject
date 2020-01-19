3e05b45ab644b597bd29f789528e73aaa1337ca0
Cindy <cindyho5241@gmail.com>
2020-01-18 23:10:07 -0800

    added aref

---
+++ form.js
@@ -0,0 +1,12 @@
+
+let data = {
+    name: "paw",
+    iMoney: 10,
+    eMoney: 100
+}
+function submit(){
+    console.log("hesgd");
+    let url = 'http://localhost:5500/results.html?data=' + JSON.stringify(data);
+    window.location = url;
+}
+
