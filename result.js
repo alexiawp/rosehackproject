c732d7e7d8c888d9eace09a49c7d758ad5106f26
arlenekimp <58005233+arlenekimp@users.noreply.github.com>
2020-01-19 10:06:18 -0800

    updated css

--- result.js
+++ result.js
@@ -19,42 +19,46 @@ function updateMoney(){
 
     if((iMoney/eMoney) < (0.1*eMoney)) {
         console.log('stage 1');
-        document.getElementById('image1').style.display = 'block';
+        document.getElementById('image').src = 'stg1.png';
     }
     else if ((iMoney/eMoney) < (0.2*eMoney) && (iMoney/eMoney) > (0.1*eMoney)) {
         console.log('stage 2');
-        document.getElementById('image2').style.display = 'block';
+        document.getElementById('image').src = 'stg2.png'; 
     }
     else if ((iMoney/eMoney) < (0.3*eMoney) && (iMoney/eMoney) > (0.2*eMoney)) {
-        // console.log('stage 3');
-        // document.getElementById('image2').style.display = 'block';
+         console.log('stage 3');
+         document.getElementById('image').style.display = 'stg3.png';
     }
     else if ((iMoney/eMoney) < (0.4*eMoney) && (iMoney/eMoney) > (0.3*eMoney)) {
-        // console.log('stage 4');
-        // document.getElementById('image2').style.display = 'block';
+         console.log('stage 4');
+         document.getElementById('image').style.display = 'stg4.png';
     }
     else if ((iMoney/eMoney) < (0.5*eMoney) && (iMoney/eMoney) > (0.4*eMoney)) {
-        // console.log('stage 5');
-        // document.getElementById('image2').style.display = 'block';
+         console.log('stage 5');
+         document.getElementById('image').style.display = 'stg5.png';
     }
     else if ((iMoney/eMoney) < (0.6*eMoney) && (iMoney/eMoney) > (0.5*eMoney)) {
-    //     console.log('stage 6');
-    //     document.getElementById('image2').style.display = 'block';
+         console.log('stage 6');
+         document.getElementById('image').style.display = 'stg6.png';
     }
     else if ((iMoney/eMoney) < (0.7*eMoney) && (iMoney/eMoney) > (0.6*eMoney)) {
-        // console.log('stage 7');
-        // document.getElementById('image2').style.display = 'block';
+         console.log('stage 7');
+         document.getElementById('image').style.display = 'stg7.png';
     }
     else if ((iMoney/eMoney) < (0.8*eMoney) && (iMoney/eMoney) > (0.7*eMoney)) {
-        // console.log('stage 8');
-        // document.getElementById('image2').style.display = 'block';
+         console.log('stage 8');
+         document.getElementById('image').style.display = 'stg8.png';
     }
     else if ((iMoney/eMoney) < (0.9*eMoney) && (iMoney/eMoney) > (0.8*eMoney)) {
-        // console.log('stage 9');
-        // document.getElementById('image2').style.display = 'block';
+         console.log('stage 9');
+         document.getElementById('image').style.display = 'stg9.png';
+    }
+    else if ((iMoney == 0)) {
+             console.log('stage 0');
+             document.getElementById('image').style.display = 'stg0.png';
     }
     else {
-        // console.log('stage 10');
-        // document.getElementById('image2').style.display = 'block';
+         console.log('stage 10');
+         document.getElementById('image').style.display = 'stg10.png';
     }
 }
